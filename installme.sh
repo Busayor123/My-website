68e58cdc3faa923285402c7505b74dbd118b85f1
Busayo Abati <abatiaisha@yahoo.com>
2024-03-09 08:37:31 -0500

    save me

---
+++ installme.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y httpd
+cd /var/www/html 
+wget https://github.com/Busayor123/web/raw/main/website-mainn.zip
+unzip website-mainn.zip
+cd website-main
+cd esigned-html
+cp -r esigned-html/*  /var/www/html
+rm -rf website-mainn.zip
+systemctl enable httpd
+systemctl start httpd
+ 
+
---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr" {
+    type = string
+  
+}
+
+variable "public_subnets" {
+ type = string 
+}
+
+variable "" {
+  
+}
