988f4091d7db733b5550085419ea7cb264405d3c
Busayo Abati <abatiaisha@yahoo.com>
2024-03-09 09:10:24 -0500

    save me

--- installme.sh
+++ installme.sh
@@ -5,7 +5,6 @@ cd /var/www/html
 wget https://github.com/Busayor123/web/raw/main/website-mainn.zip
 unzip website-mainn.zip
 cd website-main
-cd esigned-html
 cp -r esigned-html/*  /var/www/html
 rm -rf website-mainn.zip
 systemctl enable httpd
